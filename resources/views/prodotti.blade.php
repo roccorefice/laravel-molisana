7c65f6e3139117982bf0e979114c36df0730f6a5
Rocco Orefice <rocco.orefice92@gmail.com>
2021-02-11 18:37:09 +0100

    divido prodotti per tipo

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -8,9 +8,43 @@ Prodotti
 
 <!-- main -->
 @section('main_content')
-    <section class="prodotti d_flex">
-        @foreach ($db_pasta as $pasta)
+    <!-- sezione pasta lunga -->
+    <section class="prodotti" style="background-image: url('{{ asset("img/fondo-pag-speciali.jpg")}}')">
+        <h2>LE LUNGHE</h2>
+        <div class="container">
+            @foreach ($db_pasta as $pasta)
+            @if ( $pasta['tipo'] === "lunga")
             <img src=" {{ $pasta['src'] }} " alt="">
-        @endforeach
+            @endif
+            @endforeach
+            
+        </div>
     </section>
+    
+    <!-- sezione pasta corta -->
+    <section class="prodotti" style="background-image: url('{{ asset("img/fondo-pag-speciali.jpg")}}'); ">
+        <h2>LE CORTE</h2>
+        <div class="container">
+            @foreach ($db_pasta as $pasta)
+            @if ( $pasta['tipo'] === "corta")
+            <img src=" {{ $pasta['src'] }} " alt="">
+            @endif
+            @endforeach
+
+        </div>
+    </section>
+    
+    <!-- sezione pasta cortissima -->
+    <section class="prodotti" style="background-image: url('{{ asset("img/fondo-pag-speciali.jpg")}}')">
+        <h2>LE CORTISSIME</h2>
+        <div class="container">
+            @foreach ($db_pasta as $pasta)
+            @if ( $pasta['tipo'] === "cortissima")
+            <img src=" {{ $pasta['src'] }} " alt="">
+            @endif
+            @endforeach
+        </div>
+    </section>
+
+
 @endsection
