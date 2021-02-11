5460268872c01d3ad7222d697f34f3c74e44604d
Rocco Orefice <rocco.orefice92@gmail.com>
2021-02-11 16:13:15 +0100

    header comune x 3 pag home,contatti,prodotti

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    @include('layouts.head')
+    <body>
+        @include('partials.header')
+        
+    </body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <!-- logo -->
+    <div class="img_logo">
+        <img src="{{ asset('img/logo.png') }}" alt="">
+    </div>
+    
+    <!-- navbar -->
+    <nav>
+        <a href="/">Home</a>
+        <a href="prodotti">Prodotti</a>
+        <a href="contatti">Contatti</a>
+    </nav>
+</header>
